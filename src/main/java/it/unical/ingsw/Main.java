0ffb710f1708e9e7b3d2c2a8a08246bf89cfe853
maborroto <maborroto1987@gmail.com>
2024-01-31 15:12:05 +0100

    minor fix

--- src/main/java/it/unical/ingsw/Main.java
+++ src/main/java/it/unical/ingsw/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		FunnyAlgorithm m = new FunnyAlgorithm();
 		try {
 			List<Integer> list = Arrays.asList(new Integer[]{2, 3, -7});
-			System.out.println(m.findIndex(list, -7 ));
+			System.out.println(m.findIndex(list, 2 ));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
